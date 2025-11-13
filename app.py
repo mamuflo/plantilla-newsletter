0ac123d97ba0c3b60709fb8891b088be5b57f22f
mamuflo <mamuflo2@gmail.com>
2025-11-13 15:42:56 +0100

    Arreglo de fallo en Python por autenticación Drive

--- app.py
+++ app.py
@@ -359,4 +359,5 @@ def list_images_in_folder(folder_id):
 
 if __name__ == '__main__':
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+    os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
     app.run(debug=True)
